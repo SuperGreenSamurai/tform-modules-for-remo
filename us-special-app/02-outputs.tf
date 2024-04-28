96a8607dc7c45509e0e796d8bf01ff6b62de9be8
SuperGreenSamurai <144400846+SuperGreenSamurai@users.noreply.github.com>
2024-04-28 09:58:45 -0700

    Updated Outputs
    This will Output Ec2 id and Dynamo Db Name

--- us-special-app/02-outputs.tf
+++ us-special-app/02-outputs.tf
@@ -8,3 +8,12 @@ output "dynamodb_table_name" {
   value = "{../modulesspecial-app}aws_dynamodb.table_name"
 }
 */
+#output ec2 id
+output "ec2-id-post-output" {
+value = module.modulesspecial.ec2.id
+}
+
+#output dynamo db table name
+output "dynamo-db-name" {
+value = module.modulesspecial.dynamodb_table_name
+}
